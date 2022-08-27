b68092e2db96cc44bff385f1ee20c89774a1918f
Ernesto Miranda <ernomitrovic@zohomail.com>
2022-08-26 22:28:00 -0500

    Documentacion terminada

--- actOrden/main.cpp
+++ actOrden/main.cpp
@@ -3,59 +3,85 @@
 #include <string>
 #include <sstream>
 
+/**
+ * @brief Hace un ordenamiento burbuja de complejidad O(n^2)
+ * 
+ * @tparam T el objeto pasado como referencia, se ausme que puede ser un string
+ * @param names objeto con los nombres
+ * @param n tamanio del arreglo
+ */
 template <class T>
 void bubbleSort(T & names, int n) {
-  T temp, * arr = new T [n];
-  std::stringstream namesStream (names);
-  int i = 0;
-  while(std::getline(namesStream, temp)){
-    arr[i++] = temp;
+  T temp, * arr = new T [n]; // O(1)
+  std::stringstream namesStream (names); // O(1)
+  int i = 0; // O(1)
+  while(std::getline(namesStream, temp)){ // O(n)
+    arr[i++] = temp; // O(1)
   }
-  for (int i=0; i<n; i++){
-    for (int j=0; j<n-1; j++){
-      if (arr[j] > arr[j+1]){
-        temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
+  for (int i=0; i<n; i++){ // O(n^2)
+    for (int j=0; j<n-1; j++){ // O(n)
+      if (arr[j] > arr[j+1]){ // O(1)
+        temp = arr[j]; // O(1)
+        arr[j] = arr[j+1]; // O(1)
+        arr[j+1] = temp; // O(1)
       }
     }
   }
-  names = "";
-  for(int i = 0; i < n; i++){
-    names += arr[i] + "\n";
+  names = ""; // O(1)
+  for(int i = 0; i < n; i++){ // O(n)
+    names += arr[i] + "\n"; // O(1)
   }
 }
 
+/**
+ * @brief Hace la búsqueda secuencial y el resultado lo guarda en un documento llamado out.csv
+ * 
+ * @tparam T objeto que se asume que se comporta como string
+ * @param names objeto con los nombres
+ * @param n tamanio del arreglo
+ * @param search caracter a buscar
+ */
 template <class T>
 void sequentialSearch(T & names, int n, char search) {
-  T temp, * arr = new T [n];
-  std::stringstream namesStream (names);
-  int i = 0;
-  while(std::getline(namesStream, temp)){
-    arr[i++] = temp;
+  T temp, * arr = new T [n]; // O(1)
+  std::stringstream namesStream (names); // O(1)
+  int i = 0; // O(1)
+  while(std::getline(namesStream, temp)){ // O(n)
+    arr[i++] = temp; // O(1)
   }
-  std::ofstream out("out.csv");
-  for(int i=0; i <n; i++) {
-    temp = arr[i];
-    if(temp[0] == search) {
-    out << arr[i] << "\n";
+  std::ofstream out("out.csv"); // O(1)
+  for(int i=0; i <n; i++) { // O(n)
+    temp = arr[i]; // O(1)
+    if(temp[0] == search) { // O(1)
+    out << arr[i] << "\n"; // O(1)
     }
   }
-  out.close();
+  out.close(); // O(1)
 }
 
+/**
+ * @brief Cuenta las lineas del archivo
+ * 
+ * @param data referencia del stream del archivo
+ * @return int el numero de lineas
+ */
 int countLines(std::fstream & data){
-  std::string line;
-  if(data.is_open()){
-    int count = 0;
-    std::string line;
-    while(std::getline(data, line)) 
-      count++;
-    return count;
+  if(data.is_open()){ // O(1)
+    int count = 0; // O(1)
+    std::string line; // O(1)
+    while(std::getline(data, line)) // O(n)
+      count++; // O(1)
+    return count; // O(1)
   } 
-  return 0;
+  return 0; // O(1)
 }
 
+/**
+ * @brief cuenta las columnas del archivo
+ * 
+ * @param rawLine string con cada fila
+ * @return int el numero de columnas
+ */
 int countCols(std::string rawLine){
   std::stringstream str (rawLine);
   int count = 0;
@@ -65,34 +91,47 @@ int countCols(std::string rawLine){
   return count++;
 }
 
+/**
+ * @brief regresa el dato de la columna
+ * 
+ * @param rawLine string con los valores de la línea 
+ * @param INDEX indice buscado
+ * @return std::string 
+ */
 std::string getCol(std::string rawLine, const int INDEX){
-  std::stringstream str (rawLine);
-  std::string colData;
-  int currentIndex = 0;
-  while(std::getline(str, colData, ',')){
-    if(INDEX==currentIndex) return colData;
-    currentIndex++;
+  std::stringstream str (rawLine); // O(1)
+  std::string colData; // O(1)
+  int currentIndex = 0; // O(1)
+  while(std::getline(str, colData, ',')){ // O(n)
+    if(INDEX==currentIndex) return colData; // O(1)
+    currentIndex++; // O(1)
   }
-  return "";
+  return ""; // O(1)
 }
 
+/**
+ * @brief Regresa el contenido del archivo como un apuntador de strings
+ * 
+ * @param data referencia del stream del archivo
+ * @return std::string* el apuntador con el contenido del archivo
+ */
 std::string * getContent(std::fstream & data){
-  const int N = countLines(data)-1;
-  std::string rawLine;
-  data.clear();
-  data.seekg(0, data.beg);
-  std::getline(data, rawLine);
-  const int M = countCols(rawLine);
-  std::string * content = new std::string[M];
-  if(data.is_open()){
-    while(std::getline(data, rawLine)){
-      for(int i = 0; i < M; i++){
-        content[i] += getCol(rawLine, i) +"\n";
+  const int N = countLines(data)-1; // O(1)
+  std::string rawLine; // O(1)
+  data.clear(); // O(1)
+  data.seekg(0, data.beg); // O(1)
+  std::getline(data, rawLine); // O(1)
+  const int M = countCols(rawLine); // O(1)
+  std::string * content = new std::string[M]; // O(1)
+  if(data.is_open()){ // O(1)
+    while(std::getline(data, rawLine)){ // O(n)
+      for(int i = 0; i < M; i++){ // O(m)
+        content[i] += getCol(rawLine, i) +"\n"; // O(n) por la complejidad de la funcion
       }
     }
   }
-  data.close();
-  return content;
+  data.close(); // O(1)
+  return content; // O(1)
 }
 
 int main(){
